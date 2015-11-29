c743025ba1faa7630d5049ed896c0161dd139ae6
AungThiha <mr.aungthiha@gmail.com>
2015-11-29 07:05:19 +0630

    remove dict and query from db

--- finalproject.py
+++ finalproject.py
@@ -1,19 +1,22 @@
 from flask import Flask, render_template
+from database_setup import Base, Restaurant, MenuItem
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import database_setup
 
 __author__ = 'aungthiha'
 
 app = Flask(__name__)
 
-restaurant = {'name': 'The CRUDdy Crab', 'id': '1', 'address': 'address 1'}
-
-restaurants = [{'name': 'The CRUDdy Crab', 'id': '1', 'address': 'address 1'}, {'name':'Blue Burgers', 'id' : '2', 'address': 'address 2'},{'name':'Taco Hut', 'id':'3', 'address': 'address 3'}]
-
-items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
-item = {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
+engine = create_engine('sqlite:///restaurant.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 
 @app.route('/restaurants/')
 def show_restaurants():
+    restaurants = session.query(Restaurant).all()
     return render_template('restaurants.html', restaurants=restaurants)
 
 
@@ -24,33 +27,42 @@ def new_restaurant():
 
 @app.route('/restaurant/<int:restaurant_id>/edit')
 def edit_restaurant(restaurant_id):
-    return render_template('edit_restaurant.html', restaurant=restaurants[restaurant_id-1])
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    return render_template('edit_restaurant.html', restaurant=restaurant)
 
 
 @app.route('/restaurant/<int:restaurant_id>/delete')
 def delete_restaurant(restaurant_id):
-    return render_template('delete_restaurant.html', restaurant=restaurants[restaurant_id-1])
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    return render_template('delete_restaurant.html', restaurant=restaurant)
 
 
 @app.route('/restaurant/<int:restaurant_id>/')
 def show_menu(restaurant_id):
-    return render_template('menu.html', items=items, restaurant=restaurants[restaurant_id-1])
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id).all()
+    return render_template('menu.html', items=items, restaurant=restaurant)
 
 
 @app.route('/restaurant/<int:restaurant_id>/new')
 def new_menu(restaurant_id):
-    return render_template('new_menu.html', restaurant=restaurants[restaurant_id-1])
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    return render_template('new_menu.html', restaurant=restaurant)
 
 
 @app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit')
 def edit_menu(restaurant_id, menu_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id).all()
     return render_template('edit_menu.html', c_list= ['Appetizer', 'Entree', 'Dessert', 'Beverage'],
-                           item=items[menu_id-1], restaurant=restaurants[restaurant_id-1])
+                           item=items[menu_id-1], restaurant=restaurant)
 
 
 @app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete')
 def delete_menu(restaurant_id, menu_id):
-    return render_template('delete_menu.html', item=items[menu_id-1], restaurant=restaurants[restaurant_id-1])
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id).all()
+    return render_template('delete_menu.html', item=items[menu_id-1], restaurant=restaurant)
 
 
 if __name__ == '__main__':
